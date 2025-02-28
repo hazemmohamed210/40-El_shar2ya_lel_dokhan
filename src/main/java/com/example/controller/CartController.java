7f6f839fbd80a284f38e3c2921d7102e3ecabab6
hazemmohamed210 <hazemanwar349@gmail.com>
2025-02-28 16:58:25 +0200

    link service with repo & controller with service

--- src/main/java/com/example/controller/CartController.java
+++ src/main/java/com/example/controller/CartController.java
@@ -1,4 +1,59 @@
 package com.example.controller;
 
+import com.example.model.Cart;
+import com.example.model.Product;
+import com.example.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    private CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/")
+    public ArrayList<Cart> getCarts(){
+        return cartService.getCarts();
+    }
+
+    @GetMapping("/{cartId}")
+    public Cart getCartById(@PathVariable UUID cartId){
+        return cartService.getCartById(cartId);
+    }
+
+    @PutMapping("/addProduct/{cartId}")
+    public String addProductToCart(@PathVariable UUID cartId, @RequestBody Product product){
+        try {
+            cartService.addProductToCart(cartId, product);
+        } catch (Exception e){
+            e.printStackTrace();
+            return "failed to addProductToCart";
+        }
+
+        return "success";
+    }
+
+    @DeleteMapping("/delete/{cartId}")
+    public String deleteCartById(@PathVariable UUID cartId){
+        try {
+            cartService.deleteCartById(cartId);
+        } catch (Exception e){
+            e.printStackTrace();
+            return "failed to deleteCartById";
+        }
+
+        return "success";
+    }
+
+
 }
+
--- src/main/java/com/example/repository/CartRepository.java
+++ src/main/java/com/example/repository/CartRepository.java
@@ -1,4 +1,55 @@
 package com.example.repository;
 
-public class CartRepository {
+import com.example.model.Cart;
+import com.example.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class CartRepository extends MainRepository<Cart> {
+
+    public static List<Cart> carts = new ArrayList<>();
+
+    @Override
+    protected String getDataPath() {
+        return null;
+    }
+
+    @Override
+    protected Class<Cart[]> getArrayType() {
+        return null;
+    }
+
+    public Cart addCart(Cart cart){
+        return null;
+    }
+
+    public ArrayList<Cart> getCarts(){
+        return null;
+    }
+
+    public Cart getCartById(UUID cartId){
+        return null;
+    }
+
+    public Cart getCartByUserId(UUID userId){
+        return null;
+    }
+
+    public void addProductToCart(UUID cartId, Product product){
+
+    }
+
+    public void deleteProductFromCart(UUID cartId, Product product){
+
+    }
+
+    public void deleteCartById(UUID cartId){
+
+    }
 }
+
